00b5253b28d6fc15bcb61afa807daf466fa11fed
southvalhalla <ah.alejo25@gmail.com>
2023-01-31 16:59:21 -0500

    se crea componentes empleados

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { EmpleadosComponent } from './empleados/empleados.componet';
+import { EmpleadoComponent } from './empleado/empleado.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,EmpleadosComponent, EmpleadoComponent
   ],
   imports: [
     BrowserModule
