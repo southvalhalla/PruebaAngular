829a650948a7d94b02618b3e3b08177157a0b5fd
southvalhalla <ah.alejo25@gmail.com>
2023-02-01 15:42:32 -0500

    Class Binding o enlace de clases

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,11 @@ import { AppComponent } from './app.component';
 import { EmpleadosComponent } from './components/empleados/empleados.componet'; 
 import { EmpleadoComponent } from './components/empleado/empleado.component';
 import { ContadorComponent } from './components/contador/contador.component';
+import { BotonesComponent } from './botones/botones.component';
 
 @NgModule({
   declarations: [
-    AppComponent,EmpleadosComponent, EmpleadoComponent, ContadorComponent
+    AppComponent,EmpleadosComponent, EmpleadoComponent, ContadorComponent, BotonesComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/botones/botones.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-botones',
+  templateUrl: './botones.component.html',
+  styleUrls: ['./botones.component.css']
+})
+export class BotonesComponent {
+  text_color:string = "";
+
+  button_disabled:boolean = false;
+}
