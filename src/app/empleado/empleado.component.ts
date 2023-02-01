2213b158f7617a29ee57a5b9a7bffefa646dbe60
southvalhalla <ah.alejo25@gmail.com>
2023-02-01 12:03:59 -0500

    Aprendiendo interpolacion

--- src/app/empleado/empleado.component.ts
+++ src/app/empleado/empleado.component.ts
@@ -1,10 +1,28 @@
 import { Component } from '@angular/core';
 
+import { Persona } from '../interfaces/persona';
+
 @Component({
   selector: 'app-empleado',
   templateUrl: './empleado.component.html',
   styleUrls: ['./empleado.component.css']
 })
 export class EmpleadoComponent {
+  constructor(){
+
+  }
+  ngOnInit(): void{
+
+  }
+
+  // nombre:string = 'Alejandro';
+
+  //con objetos
+  
+  persona:Persona = {
+    nombre : 'Alejandro',
+    apellido : 'De Sinope',
+    edad: 21
+  }
 
 }
