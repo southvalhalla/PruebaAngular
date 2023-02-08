7217daee6763e5e0539339e5902f5bb6ba8b75f1
southvalhalla <ah.alejo25@gmail.com>
2023-02-08 13:11:04 -0500

    redistribucion de carpetas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,18 +5,20 @@ import { AppComponent } from './app.component';
 import { EmpleadosComponent } from './components/empleados/empleados.componet'; 
 import { EmpleadoComponent } from './components/empleado/empleado.component';
 import { ContadorComponent } from './components/contador/contador.component';
-import { BotonesComponent } from './botones/botones.component';
-import { FormularioComponent } from './formulario/formulario.component';
-import { SwitchComponent } from './switch/switch.component';
-import { FormularioPlantillaComponent } from './formulario-plantilla/formulario-plantilla.component';
+import { BotonesComponent } from './components/botones/botones.component';
+import { FormularioComponent } from './components/formulario/formulario.component';
+import { SwitchComponent } from './components/switch/switch.component';
+import { FormularioPlantillaComponent } from './components/formulario-plantilla/formulario-plantilla.component';
+import { FormularioReactivoComponent } from './components/formulario-reactivo/formulario-reactivo.component';
+import { ChildrenComponent } from './components/children/children.component';
+import { AddMessageComponent } from './components/add-message/add-message.component';
+import { ListMessageComponent } from './components/list-message/list-message.component';
 
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { FormularioReactivoComponent } from './formulario-reactivo/formulario-reactivo.component';
-import { ChildrenComponent } from './children/children.component';
 
 @NgModule({
   declarations: [
-    AppComponent,EmpleadosComponent, EmpleadoComponent, ContadorComponent, BotonesComponent, FormularioComponent, SwitchComponent, FormularioPlantillaComponent, FormularioReactivoComponent, ChildrenComponent
+    AppComponent,EmpleadosComponent, EmpleadoComponent, ContadorComponent, BotonesComponent, FormularioComponent, SwitchComponent, FormularioPlantillaComponent, FormularioReactivoComponent, ChildrenComponent, AddMessageComponent, ListMessageComponent
   ],
   imports: [
     BrowserModule, FormsModule, ReactiveFormsModule
